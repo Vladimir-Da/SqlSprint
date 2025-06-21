bcdd8caf283386388e7d5460651474401a391041
Владимир Данилов <Nevrak87@gmail.com>
2025-06-21 22:10:16 +0300

    next

--- parcel.go
+++ parcel.go
@@ -14,6 +14,8 @@ func NewParcelStore(db *sql.DB) ParcelStore {
 }
 
 func (s ParcelStore) Add(p Parcel) (int, error) {
+	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
+
 	res, err := s.db.Exec("INSERT INTO parcel (client, status, address,created_at) VALUES (:client, :status,:address,:created_at)",
 		sql.Named("client", p.Client),
 		sql.Named("status", p.Status),
@@ -22,14 +24,13 @@ func (s ParcelStore) Add(p Parcel) (int, error) {
 	if err != nil {
 		return 0, err
 	}
+
+	// верните идентификатор последней добавленной записи
 	id, err := res.LastInsertId()
 	if err != nil {
 		return 0, err
 	}
 	return int(id), nil
-	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
-
-	// верните идентификатор последней добавленной записи
 }
 
 func (s ParcelStore) Get(number int) (Parcel, error) {
